bf63e56f747615a9f5dc999eccb4810f60accd98
Aravind <aravindsugunan@gmail.com>
2022-12-28 06:29:26 +0100

    Adding the 2016job files

---
+++ Systematics/test/sub_Era2016_legacyPostVFP_v1_Summer19UL_data.sh
@@ -0,0 +1,9 @@
+fggRunJobs.py \
+	--load Era2016_legacyPostVFP_v1_Summer19UL_data.json \
+    -d jobs_Era2016_legacyPostVFP_v1_Summer19UL_data \
+    workspaceHHH_Data.py \
+    maxEvents=-1  \
+    -n 400 \
+    -q workday \
+	--no-copy-proxy \
+	--stage-to=/eos/home-a/athachay/workarea/trippleHiggs/flashggNtuples/v3/Era2016_legacyPostVFP_v1_Summer19UL
---
+++ Systematics/test/sub_Era2016_legacyPreVFP_v1_Summer19UL.sh
@@ -0,0 +1,9 @@
+fggRunJobs.py \
+	--load Era2016_legacyPreVFP_v1_Summer19UL.json \
+    -d jobs_Era2016_legacyPreVFP_v1_Summer19UL \
+    workspaceHHH_MC.py \
+    maxEvents=-1  \
+    -n 400 \
+    -q workday \
+	--no-copy-proxy \
+	--stage-to=/eos/home-a/athachay/workarea/trippleHiggs/flashggNtuples/v3/Era2016_legacyPreVFP_v1_Summer19UL
