bcc4630820fbf48e9a8d04f32d2e8db438e42978
Aravind <aravindsugunan@gmail.com>
2022-12-28 06:42:44 +0100

    Adding the 2017 submit scriptswqa

---
+++ Systematics/test/sub_Era2017_legacy_v1_Summer20UL_data.sh
@@ -0,0 +1,10 @@
+fggRunJobs.py \
+    --load Era2017_legacy_v1_Summer20UL_data.json \
+    -d jobs_Era2017_legacy_v1_Summer20UL_data \
+    workspaceHHH_Data.py \
+    maxEvents=-1  \
+    -n 400 \
+    -q workday \
+    --no-copy-proxy \
+	--stage-to=/eos/home-a/athachay/workarea/trippleHiggs/flashggNtuples/v3/Era2017_legacy_v1_Summer20UL_data
+
